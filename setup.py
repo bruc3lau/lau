eec06eb336ad8f962e6fc8e3a74c38b698fcd345
qxliu <qxliu@aibee.com>
2023-12-08 16:38:11 +0800

    update setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lau',
-    version='0.1.0',
+    version='0.1.1',
     author='Bruce Lau',
     author_email='lqx404@gmail.com',
     packages=find_packages(),
